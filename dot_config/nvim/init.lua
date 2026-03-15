7a10096c15c4b5adb4b3dc54c03eeda638a66246
Andrej B <abozic389@gmail.com>
2026-03-15 17:28:19 +0100

    feat: linux script wip

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,6 +1,7 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
 
 if vim.g.vscode then
   require("config.ide.vscode")
+else
+  require("config.lazy")
 end
--- dot_config/nvim/lua/config/ide/vscode.lua
+++ dot_config/nvim/lua/config/ide/vscode.lua
@@ -1,16 +1,128 @@
-if not vim.g.vscode then return end
+if not vim.g.vscode then
+  return
+end
 
-local keymap = vim.keymap
+local keymap = vim.keymap.set
+local vscode = require("vscode")
 
-keymap.set({"n", "v"}, "<leader>t", "<cmd>lua require('vscode').action('workbench.action.terminal.toggleTerminal')<CR>")
-keymap.set({"n", "v"}, "<leader>a", "<cmd>lua require('vscode').action('editor.action.quickFix')<CR>")
--- keymap.set({"n", "v"}, "<leader>sp", "<cmd>lua require('vscode').action('workbench.actions.view.problems')<CR>")
--- keymap.set({"n", "v"}, "<leader>cn", "<cmd>lua require('vscode').action('notifications.clearAll')<CR>")
-keymap.set({"n", "v"}, "<leader>ff", "<cmd>lua require('vscode').action('workbench.action.quickOpen')<CR>")
-keymap.set({"n", "v"}, "<leader>cp", "<cmd>lua require('vscode').action('workbench.action.showCommands')<CR>")
-keymap.set({"n", "v"}, "<leader>pr", "<cmd>lua require('vscode').action('code-runner.run')<CR>")
-keymap.set({"n", "v"}, "<leader>fd", "<cmd>lua require('vscode').action('editor.action.formatDocument')<CR>")
+-- ============================================================================
+-- SETTINGS (match ideavimrc)
+-- ============================================================================
 
-keymap.set({"n", "v"}, "<leader>ex", "<cmd>lua require('vscode').action('workbench.view.explorer')<CR>")
-keymap.set("n", "<Tab>", "<cmd>lua require('vscode').action('workbench.action.nextEditorInGroup')<CR>")
-keymap.set("n", "<S-Tab>", "<cmd>lua require('vscode').action('workbench.action.previousEditorInGroup')<CR>")
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+
+vim.opt.incsearch = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.hlsearch = false
+
+-- vim.opt.clipboard:append("unnamedplus")
+vim.opt.scrolloff = 8
+
+vim.g.mapleader = " "
+
+-- ============================================================================
+-- SCROLLING / SEARCH
+-- ============================================================================
+
+keymap("n", "<C-d>", "<C-d>zz")
+keymap("n", "<C-u>", "<C-u>zz")
+
+keymap("n", "n", "nzzzv")
+keymap("n", "N", "Nzzzv")
+
+-- ============================================================================
+-- VISUAL MODE IMPROVEMENTS
+-- ============================================================================
+
+keymap("v", "<", "<gv")
+keymap("v", ">", ">gv")
+
+keymap("v", "J", ":m '>+1<CR>gv=gv")
+keymap("v", "K", ":m '<-2<CR>gv=gv")
+
+-- ============================================================================
+-- CLIPBOARD
+-- ============================================================================
+
+keymap({ "n", "v" }, "<leader>y", '"+y')
+keymap("n", "<leader>p", '"+p')
+
+-- ============================================================================
+-- FILE / SEARCH
+-- ============================================================================
+
+keymap({ "n", "v" }, "<leader>ff", function()
+  vscode.action("workbench.action.quickOpen")
+end)
+
+keymap({ "n", "v" }, "<leader>fs", function()
+  vscode.action("workbench.action.findInFiles")
+end)
+
+-- ============================================================================
+-- CODE ACTIONS / REFACTORING
+-- ============================================================================
+
+keymap({ "n", "v" }, "<leader>a", function()
+  vscode.action("editor.action.quickFix")
+end)
+
+keymap({ "n", "v" }, "<leader>rn", function()
+  vscode.action("editor.action.rename")
+end)
+
+-- ============================================================================
+-- CODE NAVIGATION
+-- ============================================================================
+
+keymap("n", "gd", function()
+  vscode.action("editor.action.revealDefinition")
+end)
+
+keymap("n", "gi", function()
+  vscode.action("editor.action.goToImplementation")
+end)
+
+-- ============================================================================
+-- TERMINAL
+-- ============================================================================
+
+keymap({ "n", "v" }, "<leader>t", function()
+  vscode.action("workbench.action.terminal.toggleTerminal")
+end)
+
+-- ============================================================================
+-- TABS
+-- ============================================================================
+
+keymap("n", "<leader>tn", function()
+  vscode.action("workbench.action.nextEditor")
+end)
+
+keymap("n", "<leader>tp", function()
+  vscode.action("workbench.action.previousEditor")
+end)
+
+keymap("n", "<leader>x", function()
+  vscode.action("workbench.action.closeActiveEditor")
+end)
+
+-- ============================================================================
+-- SPLITS (match ideavimrc)
+-- ============================================================================
+
+keymap("n", "<C-w>|", function()
+  vscode.action("workbench.action.splitEditorRight")
+end)
+
+keymap("n", "<C-w>-", function()
+  vscode.action("workbench.action.splitEditorDown")
+end)
